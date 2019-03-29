6b4891a42c05af3384084fa6cda6658407b98a70
Hendrik26 <hendrik.herrdler@gmail.com>
2019-03-29 11:19:08 +0100

    Add firebase-invoice-service.

---
+++ src/app/fb-invoice.service.ts
@@ -0,0 +1,11 @@
+// responsible for connection to Firebase-DB
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FbInvoiceService {
+
+  constructor() { }
+}
