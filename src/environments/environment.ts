ac30a0474502d320e6c732ecbdc5430e8fb65be1
Hendrik26 <hendrik.herrdler@gmail.com>
2019-03-29 12:10:59 +0100

    Add firebase-config-data like "apiKey" and "authDomain" to environment.ts.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebase: {
+        apiKey: "AIzaSyDR01fxU-v6whOGpi8CB5HhfOPJPrZf7rY",
+        authDomain: "invoicemanagement-fa34a.firebaseapp.com",
+        databaseURL: "https://invoicemanagement-fa34a.firebaseio.com",
+        projectId: "invoicemanagement-fa34a",
+        storageBucket: "invoicemanagement-fa34a.appspot.com",
+        messagingSenderId: "654986020566"
+
+    }
 };
 
 /*
