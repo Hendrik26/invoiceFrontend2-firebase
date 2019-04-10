e09475abcf69fd1caa7079f95dbc9090ed7cc28e
Hendrik26 <hendrik.herrdler@gmail.com>
2019-04-11 01:30:38 +0200

    Get boolean history ('customer-history') in CustomerListComponent.ts.

--- src/app/customer-list/customer-list.component.ts
+++ src/app/customer-list/customer-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 // import {isNullOrUndefined} from 'util';
 import {Customer} from '../customer';
 import {FbInvoiceService} from '../fb-invoice.service';
@@ -16,18 +16,53 @@ export class CustomerListComponent implements OnInit {
     // Branch dev ////////
 
     // region other properties
+    customerParentId: string;
     customers: Customer[];
+    newParentCustomer = false;
+    hasReceivedCustomerParentIdError = false;
     history = false;
     showArchived = false;
 
     // endregion
 
     constructor(private fbInvoiceService: FbInvoiceService,
-                private router: Router) {
+                private router: Router,
+                private route: ActivatedRoute) {
     }
 
     ngOnInit() {
+        this.hasReceivedCustomerParentIdError = !this.hasReceivedCustomerParentId();
         this.receiveCustomers();
+    }
+
+    hasReceivedCustomerParentId(): // can NOT be deleted
+        boolean {
+        console.log('<<< Start method hasReceivedCustomerParentId()! >>>');
+        if (this.route.snapshot.paramMap.has('customerId') && this.route.snapshot.paramMap.has('newCustomer')) {
+            console.log('<<< Method hasReceivedCustomerParentId() if-branch! >>>');
+            this.customerParentId = this.route.snapshot.paramMap.get('customerId');  // get customerID???? customerId from URL
+            console.log('Parent<<<<< CustomerID ===' + this.customerParentId + ' >>>>>    ');
+            this.newParentCustomer = (this.route.snapshot.paramMap.get('newCustomer') === 'true');
+
+            const has: boolean = this.route.snapshot.paramMap.has('customer-history');
+            console.log('<<< has ===' + has + '>>>');
+            const get: string = this.route.snapshot.paramMap.get('customer-history');
+            console.log('<<< get ===' + get + '>>>');
+
+            /* const keys: string = this.route.snapshot.paramMap.keys.toString();
+            console.log('<<< keys ===' + keys + '>>>'); */
+
+            const urlToString: string = this.route.snapshot.toString();
+            console.log('<<< urlToString ===' + urlToString + '>>>');
+            this.history = (urlToString.indexOf('customer-history') != -1);
+            console.log('<<< this.history ===' + this.history + '>>>');
+
+            return true;
+        } else {
+            console.log('<<< Method hasReceivedCustomerParentId() else-branch! >>>');
+            this.customerParentId = null; // stands for the creation of a new customer
+            return false;
+        }
     }
 
     private toggleShowArchived(): void {
