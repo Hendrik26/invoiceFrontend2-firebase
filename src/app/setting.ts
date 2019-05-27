7d7a365f5aca011014a5e8e238dd316d0739d68c
Hendrik <hendrik.herdler@gmail.com>
2019-05-27 13:23:29 +0200

    Added class "Setting"

---
+++ src/app/setting.ts
@@ -0,0 +1,36 @@
+export class Setting {
+
+    headerShortAddress: string;
+    headerAddressLine01: string;
+    headerAddressLine02: string;
+    headerAddressLine03: string;
+    headerAddressLine04: string;
+    headerAddressLine05: string;
+    headerAddressLine06: string;
+    headerAddressLine07: string;
+    headerAddressLine08: string;
+    headerAddressLine09: string;
+    headerAddressLine10: string;
+    headerLocation: string;
+    creditorIdentificationNumber: string;
+    footerAddressLine01: string;
+    footerAddressLine02: string;
+    footerAddressLine03: string;
+    footerAddressLine04: string;
+    footerAddressLine05: string;
+    footerContactLine01: string;
+    footerContactLine02: string;
+    footerContactLine03: string;
+    footerContactLine04: string;
+    footerContactLine05: string;
+    footerBankAccount01: string;
+    footerBankAccount02: string;
+    footerBankAccount03: string;
+    footerBankAccount04: string;
+    footerBankAccount05: string;
+    footerTaxIdentification: string;
+    LogoUrl: string;
+    creationTime: Date;
+
+
+}
